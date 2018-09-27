85b5fede32d13fca2a1d66f96b2f544deb17793e
tyler <tyler.r.hood@gmail.com>
2018-09-27 04:32:51 -0700

    add a setItem into my handler to update the datat after 25 clicks so data will compile after refreshes

--- js/app.js
+++ js/app.js
@@ -19,11 +19,45 @@ function Item(filepath, itemName) {
   Item.allItems.push(this);
 }
 
+function addLocalStorage () {
+  if(setItems){
+    Item.allItems = JSON.parse(setItems);
+
+  }else{
+
+    addItems();
+    localStorage.setItem('items', JSON.stringify(Item.allItems));
+
+  }
+}
+
+function addItems () {
+  new Item('../img/bag.jpg', 'bag');
+  new Item('../img/banana.jpg', 'banana');
+  new Item('../img/bathroom.jpg', 'bathroom');
+  new Item('../img/boots.jpg', 'boots');
+  new Item('../img/breakfast.jpg', 'breakfast');
+  new Item('../img/bubblegum.jpg', 'bubblegum');
+  new Item('../img/chair.jpg', 'chair');
+  new Item('../img/cthulhu.jpg', 'cthulhu');
+  new Item('../img/dog-duck.jpg', 'dog-duck');
+  new Item('../img/dragon.jpg', 'dragon');
+  new Item('../img/pen.jpg', 'pen');
+  new Item('../img/pet-sweep.jpg', 'pet-sweep');
+  new Item('../img/scissors.jpg', 'scissors');
+  new Item('../img/shark.jpg', 'shark');
+  new Item('../img/sweep.png', 'sweep');
+  new Item('../img/tauntaun.jpg', 'tauntaun');
+  new Item('../img/unicorn.jpg', 'unicorn');
+  new Item('../img/usb.gif', 'usb');
+  new Item('../img/water-can.jpg', 'water-can');
+  new Item('../img/wine-glass.jpg', 'wine-glass');
+}
+
 function randomNumber() {
   for(var i = 0; i < possibleImg.length; i++) {
     var randomNumberGenerator = Math.floor(Math.random() * Item.allItems.length);
     randomNumbers.push(randomNumberGenerator);
-    console.log('created number ' + i);
 
     for(var k = 0; k < possibleImg.length; k++) {
       if ((randomNumbers.length === 2 && randomNumbers[i] === randomNumbers[0]) || (randomNumbers.length === 3 && (randomNumbers[2] === randomNumbers[0] || randomNumbers[2] === randomNumbers[1])) ||(randomNumbers[i] === lastImg[k])) {
@@ -52,6 +86,28 @@ function eventListener() {
     possibleImg[i].addEventListener('click', newProducts);
     possibleImg[i].addEventListener('click', newProducts);
     possibleImg[i].addEventListener('click', newProducts);
+  }
+}
+
+function newProducts(event) {
+
+  var imageName = event.target.alt;
+  for( var i=0; i<Item.allItems.length; i++) {
+    if(Item.allItems[i].itemName === imageName ) {
+      Item.allItems[i].clickCount++;
+      break;
+    }
+  }
+
+  totalClicks++;
+
+  if (totalClicks < 25) {
+    randomNumber();
+  } else {
+    removeListener();
+    showChart();
+    localStorage.setItem('items', JSON.stringify(Item.allItems));
+    consoleLog();
   }
 }
 
@@ -127,61 +183,15 @@ function showChart() {
   });
 }
 
-function newProducts(event) {
-
-  var imageName = event.target.alt;
-  for( var i=0; i<Item.allItems.length; i++) {
-    if(Item.allItems[i].itemName === imageName ) {
-      Item.allItems[i].clickCount++;
-      break;
-    }
-  }
-
-  totalClicks++;
-
-  if (totalClicks < 25) {
-    randomNumber();
-  } else {
-    removeListener();
-    showChart();
-  }
-}
-
-function addItems () {
-  new Item('../img/bag.jpg', 'bag');
-  new Item('../img/banana.jpg', 'banana');
-  new Item('../img/bathroom.jpg', 'bathroom');
-  new Item('../img/boots.jpg', 'boots');
-  new Item('../img/breakfast.jpg', 'breakfast');
-  new Item('../img/bubblegum.jpg', 'bubblegum');
-  new Item('../img/chair.jpg', 'chair');
-  new Item('../img/cthulhu.jpg', 'cthulhu');
-  new Item('../img/dog-duck.jpg', 'dog-duck');
-  new Item('../img/dragon.jpg', 'dragon');
-  new Item('../img/pen.jpg', 'pen');
-  new Item('../img/pet-sweep.jpg', 'pet-sweep');
-  new Item('../img/scissors.jpg', 'scissors');
-  new Item('../img/shark.jpg', 'shark');
-  new Item('../img/sweep.png', 'sweep');
-  new Item('../img/tauntaun.jpg', 'tauntaun');
-  new Item('../img/unicorn.jpg', 'unicorn');
-  new Item('../img/usb.gif', 'usb');
-  new Item('../img/water-can.jpg', 'water-can');
-  new Item('../img/wine-glass.jpg', 'wine-glass');
-}
-
-function addLocalStorage () {
-  if(setItems){
-    Item.allItems = JSON.parse(setItems);
-
-  }else{
-
-    addItems();
-    localStorage.setItem('items', JSON.stringify(Item.allItems));
-
+function consoleLog () {
+  for(var i = 0; i < Item.allItems.length; i++) {
+    console.log('Item ' + Item.allItems[i].itemName);
+    console.log('Click Count ' + Item.allItems[i].clickCount);
+    console.log('Times Displayed ' + Item.allItems[i].itemDisplay);
   }
 }
 
 addLocalStorage();
 randomNumber();
 eventListener();
+// consoleLog();
